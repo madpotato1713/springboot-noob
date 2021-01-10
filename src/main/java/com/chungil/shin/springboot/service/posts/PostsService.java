637da4f707bbc8f166432288c1ffef13270562dd
madpotato1713 <42724769+madpotato1713@users.noreply.github.com>
2021-01-11 00:30:55 +0900

    구글 연동 시작

--- src/main/java/com/chungil/shin/springboot/service/posts/PostsService.java
+++ src/main/java/com/chungil/shin/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.chungil.shin.springboot.service.posts;
 
 import com.chungil.shin.springboot.domain.posts.Posts;
 import com.chungil.shin.springboot.domain.posts.PostsRepository;
+import com.chungil.shin.springboot.web.dto.PostsListResponseDto;
 import com.chungil.shin.springboot.web.dto.PostsResponseDto;
 import com.chungil.shin.springboot.web.dto.PostsSaveRequestDto;
 import com.chungil.shin.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,9 +32,22 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.deleteById(id);
     }
 }
