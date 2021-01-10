88fcbde5d26530eb4c016a67ab80381429729dd7
madpotato1713 <42724769+madpotato1713@users.noreply.github.com>
2021-01-10 18:57:18 +0900

    예제 진행 중

---
+++ src/main/java/com/chungil/shin/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.chungil.shin.springboot.web.dto;
+
+import com.chungil.shin.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
