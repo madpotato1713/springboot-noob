c22597d62ede4bae9868d5f287b22a88b5469252
madpotato1713 <42724769+madpotato1713@users.noreply.github.com>
2021-01-11 02:10:51 +0900

    구글연동예제 완료

--- src/main/java/com/chungil/shin/springboot/web/IndexController.java
+++ src/main/java/com/chungil/shin/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.chungil.shin.springboot.web;
 
+import com.chungil.shin.springboot.config.auth.dto.SessionUser;
+import com.chungil.shin.springboot.domain.user.User;
 import com.chungil.shin.springboot.service.posts.PostsService;
 import com.chungil.shin.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userFullName", user.getName());
+        }
+
         return "index";
     }
 
